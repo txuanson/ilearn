224be559d5da29e8a4f6aab3228bdc6709aba232
mon gruvia <thaonhucute2001@gmail.com>
2021-07-24 17:20:12 +0700

    view&edit profile

--- src/frontend/src/router/index.js
+++ src/frontend/src/router/index.js
@@ -2,25 +2,29 @@ import { BrowserRouter as Router, Route } from "react-router-dom"
 import { AnimatePresence } from "framer-motion";
 import SplashRoute from "../components/animation/SplashRoute";
 import Dashboard from "../pages/Dashboard";
-
+<<<<<<< HEAD
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-
+=======
 import CourseDescription from "../pages/CourseDescription";
+<<<<<<< HEAD
 import CreateNewCourse from "../pages/CreateNewCourse";
 import Profile from "../pages/Profile";
+=======
+>>>>>>> de52ff9b0945b7ee789757c9338e2e47419f0ea6
+>>>>>>> parent of a41e529 (header)
 
 export default function MyRoute() {
     return(
         <Router>
-
+            <AnimatePresence>
 
                 <Route path="/dashboard">
                     <SplashRoute key="/dashboard">
                      <Dashboard />
                     </SplashRoute>
                 </Route>
-
+<<<<<<< HEAD
 
                 <Route exact path="/login">
                     <SplashRoute key="/login">
@@ -31,9 +35,13 @@ export default function MyRoute() {
                 <Route exact path="/register">
                     <SplashRoute key="/register">
                      <Register></Register>
+<<<<<<< HEAD
                     </SplashRoute>
                 </Route> 
 
+=======
+=======
+>>>>>>> parent of a41e529 (header)
                 <Route path="/coursedescription">
                     <SplashRoute key="/coursedescription">
                      <CourseDescription
@@ -44,8 +52,9 @@ export default function MyRoute() {
                      text = "Brief description about the course. Brief description about the course."
                      content ="## 📖 About this class"
                      />
-
+>>>>>>> de52ff9b0945b7ee789757c9338e2e47419f0ea6
                     </SplashRoute>
+<<<<<<< HEAD
                 </Route> 
 
                 <Route exact path="/CreateNewCourse">
@@ -60,6 +69,10 @@ export default function MyRoute() {
                     </SplashRoute>
                 </Route>
 
+=======
+                </Route>
+            </AnimatePresence>  
+>>>>>>> parent of a41e529 (header)
         </Router>
     )
 }
