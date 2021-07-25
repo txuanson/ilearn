f52214d82bdf6c8c47b319de9f58299c4063ff6c
txson <khatmausr502@gmail.com>
2021-07-25 17:21:59 +0700

    fix router

--- src/frontend/src/router/index.js
+++ src/frontend/src/router/index.js
@@ -1,23 +1,25 @@
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import SplashRoute from "../components/animation/SplashRoute";
 import TutorDashBoard from "../pages/TutorDashboard/TutorDashboard";
 import RegularRoute from "./regular";
 
 
 export default function MyRoute() {
-    return(
+    return (
         <Router>
-            <Route path="/tutor">
-                <SplashRoute key="/tutor">
-                    <TutorDashBoard />
-                </SplashRoute>
-            </Route>
+            <Switch>
+                <Route path="/tutor">
+                    <SplashRoute key="/tutor">
+                        <TutorDashBoard />
+                    </SplashRoute>
+                </Route>
 
-            <Route  path="/">
-                <SplashRoute key="/">
-                    <RegularRoute />
-                </SplashRoute>
-            </Route>
+                <Route path="/">
+                    <SplashRoute key="/">
+                        <RegularRoute />
+                    </SplashRoute>
+                </Route>
+            </Switch>
         </Router>
     )
 }
