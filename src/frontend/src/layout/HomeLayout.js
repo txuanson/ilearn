a9fd630863ffb94e48d9f364180031f50f531a42
phucthaii1820 <phucthai0108@gmail.com>
2021-07-25 12:02:49 +0700

    hidden message fail

--- src/frontend/src/layout/HomeLayout.js
+++ src/frontend/src/layout/HomeLayout.js
@@ -1,14 +1,12 @@
 import Header from "../components/header/Header";
+import { auth } from "../utils/auth";
 function HomeLayout({ children }) {
-    return (
-        <div>
-            <Header user = {false}></Header>
-            <div className="relative">
-                {children}
-            </div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <Header user={auth()}></Header>
+      <div className="relative">{children}</div>
+    </div>
+  );
 }
 
 export default HomeLayout;
