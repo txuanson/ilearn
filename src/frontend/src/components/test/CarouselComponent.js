fe94212ab8f67b9666958dd63d3fc0a11af5d23e
phucthaii1820 <phucthai0108@gmail.com>
2021-07-28 21:40:49 +0700

    fix carousel size xs

--- src/frontend/src/components/test/CarouselComponent.js
+++ src/frontend/src/components/test/CarouselComponent.js
@@ -1,10 +1,9 @@
-import React, { Children, Component} from "react";
+import React, { Component } from "react";
 import { Carousel, Col, Row } from "antd";
-import { ArrowRightIcon, ArrowLeftIcon} from "@heroicons/react/outline";
+import { ArrowRightIcon, ArrowLeftIcon } from "@heroicons/react/outline";
 import CardComponent from "./CardComponent";
 
-
-export default class CarouselComponent extends Component{
+export default class CarouselComponent extends Component {
   constructor(props) {
     super(props);
     this.next = this.next.bind(this);
@@ -24,78 +23,87 @@ export default class CarouselComponent extends Component{
       infinite: true,
       speed: 500,
       slidesToShow: 1,
-      slidesToScroll: 1
+      slidesToScroll: 1,
     };
     return (
-      <div className = "bg-gray-100 py-5 my-3">
-        <div className = "text-xl text-center text-blue-700 py-3 mt-3 font-bold">{this.props.name}</div>
-        <div className = "grid grid-cols-12 mb-3">
-            <ArrowLeftIcon className=" block h-10 w-6" onClick={this.previous} style = {{alignSelf:'center', justifySelf: 'center'}}></ArrowLeftIcon>
-            <div className="col-span-10" >
-                <Carousel ref={node => (this.carousel = node)} {...props}>
-                <div className="site-card-wrapper">
-                    <Row gutter={16}>
-                        <Col span={8}>
-                            <CardComponent title="Card title" bordered={false}>
-                            Card content
-                            </CardComponent>
-                        </Col>
-                        <Col span={8}>
-                            <CardComponent title="Card title" bordered={false}>
-                            Card content
-                            </CardComponent>
-                        </Col>
-                        <Col span={8}>
-                                <CardComponent title="Card title" bordered={false}>
-                                Card content
-                                </CardComponent>
-                        </Col>
-                    </Row>
-                </div>
+      <div className="bg-gray-100 py-5 my-3">
+        <div className="text-xl text-center text-blue-700 py-3 mt-3 font-bold">
+          {this.props.name}
+        </div>
+        <div className="grid grid-cols-12 mb-3">
+          <ArrowLeftIcon
+            className=" block h-10 w-6"
+            onClick={this.previous}
+            style={{ alignSelf: "center", justifySelf: "center" }}
+          ></ArrowLeftIcon>
+          <div className="col-span-10">
+            <Carousel ref={(node) => (this.carousel = node)} {...props}>
+              <div className="site-card-wrapper">
+                <Row gutter={16}>
+                  <Col xl={8} md={8} xs={12}>
+                    <CardComponent title="Card title" bordered={false}>
+                      Card content
+                    </CardComponent>
+                  </Col>
+                  <Col xl={8} md={8} xs={12}>
+                    <CardComponent title="Card title" bordered={false}>
+                      Card content
+                    </CardComponent>
+                  </Col>
+                  <Col xl={8} md={8} xs={0}>
+                    <CardComponent title="Card title" bordered={false}>
+                      Card content
+                    </CardComponent>
+                  </Col>
+                </Row>
+              </div>
 
-                <div className="site-card-wrapper">
-                    <Row gutter={16}>
-                        <Col span={8}>
-                            <CardComponent title="Card title" bordered={false}>
-                            Card content
-                            </CardComponent>
-                        </Col>
-                        <Col span={8}>
-                            <CardComponent title="Card title" bordered={false}>
-                            Card content
-                            </CardComponent>
-                        </Col>
-                        <Col span={8}>
-                            <CardComponent title="Card title" bordered={false}>
-                            Card content
-                            </CardComponent>
-                        </Col>
-                    </Row>
-                </div>
+              <div className="site-card-wrapper">
+                <Row gutter={16}>
+                  <Col xl={8} md={8} xs={12}>
+                    <CardComponent title="Card title" bordered={false}>
+                      Card content
+                    </CardComponent>
+                  </Col>
+                  <Col xl={8} md={8} xs={12}>
+                    <CardComponent title="Card title" bordered={false}>
+                      Card content
+                    </CardComponent>
+                  </Col>
+                  <Col xl={8} md={8} xs={0}>
+                    <CardComponent title="Card title" bordered={false}>
+                      Card content
+                    </CardComponent>
+                  </Col>
+                </Row>
+              </div>
 
-                <div className="site-card-wrapper">
-                    <Row gutter={16}>
-                        <Col span={8}>
-                            <CardComponent title="Card title" bordered={false}>
-                            Card content
-                            </CardComponent>
-                        </Col>
-                        <Col span={8}>
-                            <CardComponent title="Card title" bordered={false}>
-                            Card content
-                            </CardComponent>
-                        </Col>
-                        <Col span={8}>
-                            <CardComponent title="Card title" bordered={false}>
-                            Card content
-                            </CardComponent>
-                        </Col>
-                    </Row>
-                </div>
-
-                </Carousel>
-            </div>
-            <ArrowRightIcon className="block h-6 w-6" onClick={this.next} style = {{alignSelf:'center', justifySelf: 'center'}}></ArrowRightIcon>
+              <div className="site-card-wrapper">
+                <Row gutter={16}>
+                  <Col xl={8} md={8} xs={12}>
+                    <CardComponent title="Card title" bordered={false}>
+                      Card content
+                    </CardComponent>
+                  </Col>
+                  <Col xl={8} md={8} xs={12}>
+                    <CardComponent title="Card title" bordered={false}>
+                      Card content
+                    </CardComponent>
+                  </Col>
+                  <Col xl={8} md={8} xs={0}>
+                    <CardComponent title="Card title" bordered={false}>
+                      Card content
+                    </CardComponent>
+                  </Col>
+                </Row>
+              </div>
+            </Carousel>
+          </div>
+          <ArrowRightIcon
+            className="block h-6 w-6"
+            onClick={this.next}
+            style={{ alignSelf: "center", justifySelf: "center" }}
+          ></ArrowRightIcon>
         </div>
       </div>
     );
