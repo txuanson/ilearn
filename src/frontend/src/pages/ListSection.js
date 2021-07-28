92ddf0be2a8baf69b1851d19972dd48c49cafc7f
thisislins <lephuonglinh169@gmail.com>
2021-07-28 23:17:29 +0700

    Update list section

--- src/frontend/src/pages/ListSection.js
+++ src/frontend/src/pages/ListSection.js
@@ -2,10 +2,11 @@ import { Breadcrumb, Layout} from "antd";
 import React, {useState} from "react";
 import { Link} from "react-router-dom";
 import {SearchOutlined } from "@ant-design/icons";
-import { Table, Space, Switch} from 'antd';
-const { Column} = Table;
-const { Content} = Layout;
+import 'antd/dist/antd.css';
+import { Table, Input, Button, Space, Switch } from 'antd';
+import Highlighter from 'react-highlight-words';
 
+const { Content} = Layout;
 const data = [
     {
       topic: 'Introduction',
@@ -33,57 +34,128 @@ const data = [
     },
   ];
 
-  
-export default function ListSection(){
-    const [value, setValue] = useState('');
-    const onSubmit=(event) => {
-        event.preventDefault();
-        console.log(value);
-    }
+export default function ListSection() {
+    const [searchText, setsearchText] = useState('');
+    const [searchedColumn, setsearchedColumn] = useState('');
     const onChange = (checked, index) => {
         console.log('data', index)
         console.log(`switch to ${checked}`);
     };
-   return(
 
-    <Layout className="px-0 md:px-4 lg:px-20">
-        <Breadcrumb style={{ margin: '0 0 10px' }}>
-            <Breadcrumb.Item>  
-                <Link to="/homepage">iLearn</Link>
-            </Breadcrumb.Item>
-            <Breadcrumb.Item>Sections</Breadcrumb.Item>
-        </Breadcrumb>
-        <div className="text-end"> 
-            <form className="flex my-2 md:w-full justify-center">
-                <input type="text" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-2 bg-white text-gray-700 text-base"
-                onChange={event => setValue(event.target.value)}/>
-                <button className="ml-2 px-4 py-2 text-base font-semibold text-white bg-blue-500 rounded-lg shadow-md hover:bg-blue-700" type="submit"
-                onClick={onSubmit}>
-                    <SearchOutlined />
-                </button>
-            </form>
-        </div>
-        <Content>
-            <Table dataSource={data}>   
-                <Column title="Topic" dataIndex="topic" key="topic" />
-                <Column title="Content" dataIndex="content" key="content" />
-                <Column title="Duration" dataIndex="duration" key="duration" />
-                <Column title="Start time" dataIndex="start_time" key="start_time" />
-                <Column title="Visible" dataIndex="visible" key="visible"
-                render={()=>(
-                    <Switch onChange={onChange} defaultChecked={data.visible}  /> 
-                )}> 
-                </Column>
-                <Column title="Action" key="action"
-                render={() => (
-                    <Space size="middle">
-                        <Link to={`tutors/course/section/edit`}>Edit</Link>
-                        <Link to={`tutors/course/section/delete`}>Delete</Link>
-                    </Space>
-                )}
-                />
-            </Table>
-        </Content>
-    </Layout>
-   );
+  const getColumnSearchProps = dataIndex => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={node => {
+            const searchInput = node;
+          }}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{ marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button onClick={() => handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+            Reset
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: filtered => <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />,
+    onFilter: (value, record) =>
+      record[dataIndex]
+        ? record[dataIndex].toString().toLowerCase().includes(value.toLowerCase())
+        : '',
+    render: text =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
+
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setsearchText(selectedKeys[0]);
+    setsearchedColumn(dataIndex);
+  };
+
+  const handleReset = clearFilters => {
+    clearFilters();
+    setsearchText('');
+  };
+  const columns = [
+    {
+      title: 'Topic',
+      dataIndex: 'topic',
+      key: 'topic',
+      width: '30%',
+      ...getColumnSearchProps('topic'),
+    },
+    {
+      title: 'Content',
+      dataIndex: 'content',
+      key: 'content',
+      width: '20%',
+      ...getColumnSearchProps('content'),
+    },
+    {
+      title: 'Duration',
+      dataIndex: 'duration',
+      key: 'duration',
+      ...getColumnSearchProps('duration'),
+    },
+    {
+        title: 'Start time',
+        dataIndex: 'start_time',
+        key: 'start_time',
+        ...getColumnSearchProps('start_time'),
+    },
+    {
+        title: 'Visible',
+        dataIndex: 'visible',
+        key: 'visible',
+        render: ()=>(
+            <Switch onChange={onChange} defaultChecked={data.visible}  /> 
+        )
+    },
+    {
+        title: 'Action',
+        key: 'action',
+        render: () => (
+            <Space size="middle">
+                <Link to={`tutors/course/section/edit`}>Edit</Link>
+                <Link to={`tutors/course/section/delete`}>Delete</Link>
+            </Space>
+        ),
+      }
+  ];
+    return (
+        <Layout className="container mx-auto xl:px-40">
+            <Breadcrumb style={{ margin: '0 0 10px' }}>
+                <Breadcrumb.Item>  
+                    <Link to="/homepage">iLearn</Link>
+                </Breadcrumb.Item>
+                <Breadcrumb.Item>Sections</Breadcrumb.Item>
+            </Breadcrumb>
+            <Content className="my-10">
+                <Table dataSource={data} pagination={false} scroll={{ x: 'fit-content' }} columns={columns}>   
+                </Table>
+            </Content>
+        </Layout>
+    );
 }
