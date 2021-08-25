73d7d116923e2b73c80e0f7458b4bdbdd1ef2a9c
unknown <phucthai0108@gmail.com>
2021-08-25 10:38:16 +0700

    fix pagination page

--- src/frontend/src/components/Category/Category.js
+++ src/frontend/src/components/Category/Category.js
@@ -6,20 +6,35 @@ import { getCategoryID } from "../../api/homePage";
 
 export default function Category({ idCategory, nameCategory }) {
   const [data, setData] = useState([]);
+  const [minValue, setMinValue] = useState([]);
+  const [maxValue, setMaxValue] = useState([]);
+  const pageSize = 20;
   useEffect(async () => {
     try {
       const res = await getCategoryID(idCategory);
       setData(res.items);
+      setMinValue(0);
+      setMaxValue(pageSize);
     } catch (err) {
       console.log(idCategory);
     }
   }, []);
+  
+  const handleChange = (value) => {
+    if (value <= 1) {
+      setMinValue(0);
+      setMaxValue(pageSize);
+    } else {
+      setMinValue((value - 1) * pageSize);
+      setMaxValue(value * pageSize);
+    }
+  };
   return (
     <div className="relative">
       <div class="container mx-auto xl:px-40">
         <div className="p-4 pb-0 font-bold text-lg">Category / {nameCategory}</div>
         <Row>
-          {data.map((item) => (
+          {data.slice(minValue, maxValue).map((item) => (
             <Col xl={6} md={6}>
               <div className="p-2">
                 <CardComponent
@@ -30,10 +45,9 @@ export default function Category({ idCategory, nameCategory }) {
             </Col>
           ))}
         </Row>
-        
-          <div className="p-3 grid justify-items-end">
-            <Pagination defaultCurrent={1} total={50} />
-          </div>
+        <div className="p-3 grid justify-items-end">
+            <Pagination defaultCurrent={1} total={data.length} onChange={handleChange} pageSize={pageSize}/>
+          </div>)
        
       </div>
     </div>
