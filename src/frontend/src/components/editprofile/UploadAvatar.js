f480e5634440b6b71d9964bfa1cc5dd9ba724a2f
mon gruvia <thaonhucute2001@gmail.com>
2021-08-27 04:19:02 +0700

    api put avatar

--- src/frontend/src/components/editprofile/UploadAvatar.js
+++ src/frontend/src/components/editprofile/UploadAvatar.js
@@ -1,6 +1,8 @@
 // import React, { Component } from 'react';
 // import { Upload, message, Button } from 'antd';
 // import { LoadingOutlined, PlusOutlined, UploadOutlined } from '@ant-design/icons';
+// import { putAvatar } from '../../api/user';
+// import { image } from '@uiw/react-md-editor';
 
 // function getBase64(img, callback) {
 //     const reader = new FileReader();
@@ -45,16 +47,22 @@
 //               // Get this url from response in real world.
 //               getBase64(info.file.originFileObj, imageUrl =>
 //                 this.setState({
-//                   image: "",
+//                   image: imageUrl,
 //                   loading: false,
 //                 }),
+//                 console.log(this.state)
 //               );
 //             }
 //         };
 
-//         this.onUpload = () => {
+    // this.onUpload = async (event) => {
+    //   try {
+    //     const res = await putAvatar({avatar: this.state.image});
+    //   } catch (error) {
+    //     console.log("fail: ", error);
+    //   }
 
-//         }
+//     };
 
 //         return (
 //             <div className="flex flex-col items-center">
@@ -70,14 +78,101 @@
 //                     >
 //                         {imageUrl ? <img src={this.image} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
 //                     </Upload>
-//                 </div>
+            //     </div>
 
-//                 <Button type="link" onUpload = {this.onUpload}> 
-//                     <UploadOutlined /> Upload Image
-//                 </Button>
-//             </div>
+            //     <Button type="link" onUpload = {this.onUpload}> 
+            //         <UploadOutlined /> Upload Image
+            //     </Button>
+            // </div>
 //         )
 //     }
 // }
 
 // export default UploadAvatar
+
+
+import React, { useState } from 'react'
+import { UploadOutlined } from '@ant-design/icons';
+import { Upload, message, Button } from 'antd';
+import ImgCrop from "antd-img-crop";
+
+import { putAvatar } from '../../api/user';
+
+function UploadAvatar() {
+
+    const [image, setImage] = useState('');
+    const [avatar, setAvatar] = useState('');
+
+    const handleAddImage = (file) => {
+        setAvatar(file);
+        return new Promise(async (res, rej) => {
+            let src = file.url
+            if (!src) {
+                src = await new Promise((resolve) => {
+                    const reader = new FileReader()
+                    reader.readAsDataURL(file)
+                    reader.onload = () => resolve(reader.result)
+                })
+            }
+            if (src) {
+                setImage(src)
+            }
+        })
+    }
+
+    const beforeUpload = (file) => {
+        const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png'
+        if (!isJpgOrPng) {
+            message.error('Only accept JPG/PNG file!')
+        }
+        const isLt7M = file.size / 1024 / 1024 < 7
+        if (!isLt7M) {
+            message.error('Image file size must be < 7MB')
+        }
+        return isJpgOrPng && isLt7M
+    }
+
+    const onUpload = async (event) => {
+        try {
+            const res = await putAvatar({avatar: avatar});           
+        } catch (error) {
+            console.log("fail: ", error);
+        }
+    };
+
+    return (
+        <div className="flex flex-col items-center">
+            <div>
+                <ImgCrop aspect={1} quality={1}>
+                    <Upload
+                        showUploadList={false}
+                        accept="image/png, image/jpeg"
+                        action={handleAddImage}
+                        multiple={false}
+                        beforeUpload={beforeUpload}
+                    >
+                        <div
+                            className="flex rounded border-dotted border-blue-300 border-2 items-center justify-center overflow-hidden"
+                            style={{ width: 200, height: 200 }}>
+                            {!image && (
+                                <p className="text-lg color-blue">
+                                    <UploadOutlined />
+                                </p>
+                            )}
+                            { image && <img src={image} />}
+                        </div>
+                    </Upload>
+                </ImgCrop>
+            </div>
+
+            <div>
+                <Button type="link" onClick = {onUpload}> 
+                    <UploadOutlined /> Upload Image
+                </Button>
+            </div>
+            
+        </div>
+    )
+}
+
+export default UploadAvatar
