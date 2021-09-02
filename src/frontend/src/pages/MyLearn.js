82f3e1c4f54392693e1b9bcbd14777f51e75dfd8
phucthaii1820 <phucthai0108@gmail.com>
2021-09-02 22:49:42 +0700

    fix my Learn

--- src/frontend/src/pages/MyLearn.js
+++ src/frontend/src/pages/MyLearn.js
@@ -39,7 +39,7 @@ export default function MyLearn() {
         <div className="p-4 pb-0 font-bold text-lg">History</div>
         <Row>
           {data.slice(minValue, maxValue).map((item) => (
-            <Col xl={6} md={6}>
+            <Col xl={6} lg={6} md={8} xs={12} sm={12}>
               <div className="p-2">
                 <CardHistory
                   dataCard={item}
