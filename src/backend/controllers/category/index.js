77281ed106f52afb6b04932f65e85a2235c0c505
txson <khatmausr502@gmail.com>
2021-09-06 11:47:37 +0700

    sort category

--- src/backend/controllers/category/index.js
+++ src/backend/controllers/category/index.js
@@ -4,6 +4,7 @@ const Category = require("../../models/Category");
 const getCategory = asyncCatch(async (req, res, next) => {
     const items = await Category.find()
     .select("name")
+    .sort({name: 1})
     .lean()
     .exec();
     res.send(items); 
