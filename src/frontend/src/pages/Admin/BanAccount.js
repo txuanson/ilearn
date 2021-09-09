5efb33e904cc9ed1aa65ac3e3fd3119693299eb8
phucthaii1820 <phucthai0108@gmail.com>
2021-09-09 21:03:32 +0700

    fix ban

--- src/frontend/src/pages/Admin/BanAccount.js
+++ src/frontend/src/pages/Admin/BanAccount.js
@@ -16,8 +16,10 @@ export default function BanAccount({ id, fetch, valuePagination, Ban }) {
     try {
       const res = await banAccount({ amount: dayDuration, user_id: id });
       setVisible(false);
-      message.success("Ban account successfully!");
+      if (Ban) message.success("Unban account successfully!");
+      else message.success("Ban account successfully!");
       fetch("", valuePagination);
+      setDayDuration(-1);
     } catch (error) {
       handleErrorApi(error);
     }
@@ -26,29 +28,42 @@ export default function BanAccount({ id, fetch, valuePagination, Ban }) {
   return (
     <>
       {Ban ? (
-        <Button type="primary" onClick={() => setVisible(true)}>
-          Unban
-        </Button>
+        <>
+          <Button type="primary" onClick={() => setVisible(true)}>
+            Unban
+          </Button>
+          <Modal
+            title="Unban Account"
+            centered
+            visible={visible}
+            onOk={() => onBan()}
+            onCancel={() => setVisible(false)}
+            okText="Confirm"
+            width={500}
+          ><h1 className = "text-center ">Are you sure you want to unban?</h1></Modal>
+        </>
       ) : (
-        <Button type="primary" danger onClick={() => setVisible(true)}>
-          Ban
-        </Button>
+        <>
+          <Button type="primary" danger onClick={() => setVisible(true)}>
+            Ban
+          </Button>
+          <Modal
+            title="Ban Account"
+            centered
+            visible={visible}
+            onOk={() => onBan()}
+            onCancel={() => setVisible(false)}
+            okText="Confirm"
+            width={500}
+          >
+            <Input
+              placeholder="Ban duration"
+              value={dayDuration}
+              onChange={ChangeHandler}
+            />
+          </Modal>
+        </>
       )}
-      <Modal
-        title="Ban Account"
-        centered
-        visible={visible}
-        onOk={() => onBan()}
-        onCancel={() => setVisible(false)}
-        okText="Confirm"
-        width={500}
-      >
-        <Input
-          placeholder="Ban duration"
-          value={dayDuration}
-          onChange={ChangeHandler}
-        />
-      </Modal>
     </>
   );
 }
