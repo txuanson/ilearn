e4a4b46f999a1d5ea97c9e341d9a4f56def6142a
phucthaii1820 <phucthai0108@gmail.com>
2021-09-09 22:25:15 +0700

    fix dashboard admin

--- src/frontend/src/pages/Admin/AdminDashboard.js
+++ src/frontend/src/pages/Admin/AdminDashboard.js
@@ -1,4 +1,4 @@
-import { Breadcrumb, Layout, Table, Card, Col, Row} from "antd";
+import { Breadcrumb, Layout, Table, Card, Col, Row } from "antd";
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import Column from "antd/lib/table/Column";
@@ -27,108 +27,126 @@ import moment from "moment";
 //     },
 //   ];
 
-
 export default function AdminDashBoard(props) {
-    const [accountCount, setAccountCount] = useState(0);
-    const [courseCount, setCourseCount] = useState(0);
-    const [newAccountCount, setNewAccountCount] = useState(0);
-    const [newCourses, setNewCourses] = useState([]);
-    const [newCoursesCount, setNewCoursesCount] = useState(0);
+  const [accountCount, setAccountCount] = useState(0);
+  const [courseCount, setCourseCount] = useState(0);
+  const [newAccountCount, setNewAccountCount] = useState(0);
+  const [newCourses, setNewCourses] = useState([]);
+  const [newCoursesCount, setNewCoursesCount] = useState(0);
 
-    const columns = [
-        {
-          title: "Name",
-          dataIndex: "name",
-          key: "name",
-        },
-        {
-          title: "Category",
-          dataIndex: "category",
-          key: "category",
-          render: category => (
-            category.name
-            )
-        },
-        {
-          title: "Create at",
-          dataIndex: "create_at",
-          key: "create_at",
-          render: create_at => (
-            moment(create_at).fromNow()
-            )
-        },
-        {
-          title: "Subscribers",
-          dataIndex: "subscriber",
-          key: "subscriber",
-        },
-      ];
-    
-    const fetchDashboard = async () => {
-        try {
-          const res = await getDashboard();
-          //const {account_count, course_count, new_account_count, new_courses, new_courses_count} = res;
-          setAccountCount(res.account_count);
-          setCourseCount(res.course_count);
-          setNewAccountCount(res.new_account_count);
-          setNewCourses(res.new_course);
-          setNewCoursesCount(res.new_course_count);
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Category",
+      dataIndex: "category",
+      key: "category",
+      render: (category) => category.name,
+    },
+    {
+      title: "Create at",
+      dataIndex: "create_at",
+      key: "create_at",
+      render: (create_at) => moment(create_at).fromNow(),
+    },
+    {
+      title: "Subscribers",
+      dataIndex: "subscriber",
+      key: "subscriber",
+    },
+  ];
 
-          newCourses.map((items) => {
-            items["nameCategory"] = items.category.name;
-            //items.category = items.category.name;
-            return items;
-          });
-          console(newCourses);
-        } catch (err) {
-          handleErrorApi(err);
-        }
-      };
-      useEffect(() => {
-        fetchDashboard();
-    }, [])
-    return (
-        <Layout>
-            <Breadcrumb style={{ margin: '10px 0' }}>
-                <Breadcrumb.Item>
-                    <Link to="/">iLearn</Link>
-                </Breadcrumb.Item>
-                <Breadcrumb.Item>
-                    <Link to="/admin">Admin</Link>
-                </Breadcrumb.Item>
-                <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
-            </Breadcrumb>
-            <div className="site-card-wrapper my-5 flex md:flex-row flex-col">
-                <Card title="Accounts" bordered={false} style={{backgroundColor:'#FA306C', color:'#fff'}} className="text-center md:p-5 md:mx-2 mb-2">
-                    <span className="text-3xl font-bold">{accountCount}</span>
-                    <p>Total number of accounts</p>
-                </Card>
-                <Card title="Accounts" bordered={false} style={{backgroundColor:'#A25DFB', color:'#fff'}} className="text-center md:p-5 md:mx-2 mb-2">
-                    <span className="text-3xl font-bold">{newAccountCount}</span>
-                    <p>New accounts this month</p>
-                </Card>
-                <Card title="Courses" bordered={false} style={{backgroundColor:'#51AD97', color:'#fff'}} className="text-center md:p-5 md:mx-2 mb-2">
-                    <span className="text-3xl font-bold">{courseCount}</span>
-                    <p>Total number of courses</p>
-                </Card>
-                <Card title="Courses" bordered={false} style={{backgroundColor:'#047D8D', color:'#fff'}} className="text-center md:p-5 md:mx-2 mb-2">
-                    <span className="text-3xl font-bold">{newCoursesCount}</span>
-                    <p>New courses this month</p>
-                </Card>
-            </div>
-            {/* <Table dataSource={newCourses} pagination={false} scroll={{ x: 'fit-content' }}>
+  const fetchDashboard = async () => {
+    try {
+      const res = await getDashboard();
+      //const {account_count, course_count, new_account_count, new_courses, new_courses_count} = res;
+      setAccountCount(res.account_count);
+      setCourseCount(res.course_count);
+      setNewAccountCount(res.new_account_count);
+      setNewCourses(res.new_course);
+      setNewCoursesCount(res.new_course_count);
+
+      newCourses.map((items) => {
+        items["nameCategory"] = items.category.name;
+        //items.category = items.category.name;
+        return items;
+      });
+      console(newCourses);
+    } catch (err) {
+      handleErrorApi(err);
+    }
+  };
+  useEffect(() => {
+    fetchDashboard();
+  }, []);
+  return (
+    <Layout>
+      <Breadcrumb style={{ margin: "10px 0" }}>
+        <div >
+          <Breadcrumb.Item>
+            <Link to="/">iLearn</Link>
+          </Breadcrumb.Item>
+          <Breadcrumb.Item>
+            <Link to="/admin">Admin</Link>
+          </Breadcrumb.Item>
+          <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
+        </div>
+      </Breadcrumb>
+
+      <div className="site-card-wrapper my-5 flex md:flex-row flex-col" style={{ alignSelf: "center" }}>
+        <Card
+          title="Accounts"
+          bordered={false}
+          style={{ backgroundColor: "#FA306C", color: "#fff" }}
+          className="text-center md:p-5 md:mx-2 mb-2"
+        >
+          <span className="text-3xl font-bold">{accountCount}</span>
+          <p>Total number of accounts</p>
+        </Card>
+        <Card
+          title="Accounts"
+          bordered={false}
+          style={{ backgroundColor: "#A25DFB", color: "#fff" }}
+          className="text-center md:p-5 md:mx-2 mb-2"
+        >
+          <span className="text-3xl font-bold">{newAccountCount}</span>
+          <p>New accounts this month</p>
+        </Card>
+        <Card
+          title="Courses"
+          bordered={false}
+          style={{ backgroundColor: "#51AD97", color: "#fff" }}
+          className="text-center md:p-5 md:mx-2 mb-2"
+        >
+          <span className="text-3xl font-bold">{courseCount}</span>
+          <p>Total number of courses</p>
+        </Card>
+        <Card
+          title="Courses"
+          bordered={false}
+          style={{ backgroundColor: "#047D8D", color: "#fff" }}
+          className="text-center md:p-5 md:mx-2 mb-2"
+        >
+          <span className="text-3xl font-bold">{newCoursesCount}</span>
+          <p>New courses this month</p>
+        </Card>
+      </div>
+      {/* <Table dataSource={newCourses} pagination={false} scroll={{ x: 'fit-content' }}>
                 <Column title="Course name" dataIndex={"name"} key="name" />
                 <Column title="Category" dataIndex="nameCategory" key="nameCategory" />
                 <Column title="Create at" dataIndex={"create_at"} key="create_at" />
                 <Column title="Subscribers" dataIndex={"subscriber"} key="subscriber" />
             </Table> */}
 
-<Table
+      <Table
         dataSource={newCourses}
         pagination={false}
         scroll={{ x: "fit-content" }}
         columns={columns}
       ></Table>
-        </Layout>
-    );
+    </Layout>
+  );
 }
