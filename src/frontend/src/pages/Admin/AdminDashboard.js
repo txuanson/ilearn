f2afa6aaa53760a2cad6eee3a74e5e4457dc9bca
thisislins <lephuonglinh169@gmail.com>
2021-09-10 08:08:21 +0700

    Fix card display

--- src/frontend/src/pages/Admin/AdminDashboard.js
+++ src/frontend/src/pages/Admin/AdminDashboard.js
@@ -1,31 +1,10 @@
 import { Breadcrumb, Layout, Table, Card, Col, Row } from "antd";
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import Column from "antd/lib/table/Column";
-import Search from "antd/lib/input/Search";
 import { getDashboard } from "../../api/admin";
 import handleErrorApi from "../../utils/handleErrorApi";
 import moment from "moment";
-// const new_courses = [
-//     {
-//       course_name: 'Introduction',
-//       create_at: '1st Jan 2022',
-//       category: 'C++',
-//       subscribers: '100',
-//     },
-//     {
-//     course_name: 'Introduction 2',
-//     create_at: '3rd Jan 2022',
-//     category: 'C++',
-//     subscribers: '100',
-//     },
-//     {
-//     course_name: 'Introduction 3',
-//     create_at: '5th Jan 2022',
-//     category: 'C++',
-//     subscribers: '100',
-//     },
-//   ];
+
 
 export default function AdminDashBoard(props) {
   const [accountCount, setAccountCount] = useState(0);
@@ -96,14 +75,14 @@ export default function AdminDashBoard(props) {
         </div>
       </Breadcrumb>
 
-      <div className="site-card-wrapper my-5 flex md:flex-row flex-col" style={{ alignSelf: "center" }}>
+      <div className="site-card-wrapper my-5 flex md:flex-row flex-col justify-between md:p-3" >
         <Card
           title="Accounts"
           bordered={false}
           style={{ backgroundColor: "#FA306C", color: "#fff" }}
           className="text-center md:p-5 md:mx-2 mb-2"
         >
-          <span className="text-3xl font-bold">{accountCount}</span>
+          <span className="text-4xl font-bold">{accountCount}</span>
           <p>Total number of accounts</p>
         </Card>
         <Card
@@ -112,7 +91,7 @@ export default function AdminDashBoard(props) {
           style={{ backgroundColor: "#A25DFB", color: "#fff" }}
           className="text-center md:p-5 md:mx-2 mb-2"
         >
-          <span className="text-3xl font-bold">{newAccountCount}</span>
+          <span className="text-4xl font-bold">{newAccountCount}</span>
           <p>New accounts this month</p>
         </Card>
         <Card
@@ -121,7 +100,7 @@ export default function AdminDashBoard(props) {
           style={{ backgroundColor: "#51AD97", color: "#fff" }}
           className="text-center md:p-5 md:mx-2 mb-2"
         >
-          <span className="text-3xl font-bold">{courseCount}</span>
+          <span className="text-4xl font-bold">{courseCount}</span>
           <p>Total number of courses</p>
         </Card>
         <Card
@@ -130,7 +109,7 @@ export default function AdminDashBoard(props) {
           style={{ backgroundColor: "#047D8D", color: "#fff" }}
           className="text-center md:p-5 md:mx-2 mb-2"
         >
-          <span className="text-3xl font-bold">{newCoursesCount}</span>
+          <span className="text-4xl font-bold">{newCoursesCount}</span>
           <p>New courses this month</p>
         </Card>
       </div>
